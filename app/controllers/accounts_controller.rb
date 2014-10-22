f7e820c0aadee7c00a76165ffa5401882a92ec1b
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-10-22 16:05:19 -0400

    sets up account controller and form

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,24 @@
 class AccountsController < ApplicationController
+  #before_action: :authenticate_user!
+
+
+  def home
+  end
+
+  def index
+    @accounts = Account.all
+    @account = Account.new
+  end
+
+
+  private
+
+  def set_account
+    @account = Account.find(params[:id])
+  end
+
+  def account_params
+    params.require(:account).permit(:mailings, :holiday_mailings, :on_website, :company, :account_number, :restrictions, :credit, :state, :whose_account, :last_order, :sublimes_sold, :new_sublimes, :sublimes_samples, :sublimes_holder, :sublimes_poster, :sublimes_sticker, :shb_sold, :shb_samples, :rdlf_sold, :fuerte_old_sold, :fuerte_rico_sold, :anniversario_sold, :remarks, :name, :phone, :fax, :fl_resale_cert)
+  end
+
 end
