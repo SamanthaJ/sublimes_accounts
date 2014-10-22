a9ab99193f93e6c0c2dfdab36f5e4ff8702bbd75
Samantha Jaromirski <samanthajaromirski@gmail.com>
2014-10-22 14:33:09 -0400

    creates accounts resource

---
+++ db/migrate/20141022183255_create_accounts.rb
@@ -0,0 +1,35 @@
+class CreateAccounts < ActiveRecord::Migration
+  def change
+    create_table :accounts do |t|
+      t.string :mailings
+      t.string :holiday_mailings
+      t.string :on_website
+      t.string :company
+      t.integer :account_number
+      t.string :restrictions
+      t.string :credit
+      t.string :state
+      t.string :whose_account
+      t.date :last_order
+      t.integer :sublimes_sold
+      t.string :new_sublimes
+      t.string :sublimes_samples
+      t.string :sublimes_holder
+      t.string :sublimes_poster
+      t.string :sublimes_sticker
+      t.integer :shb_sold
+      t.string :shb_samples
+      t.integer :rdlf_sold
+      t.integer :fuerte_old_sold
+      t.integer :fuerte_rico_sold
+      t.integer :40anniver_sold
+      t.text :remarks
+      t.string :name
+      t.integer :phone
+      t.integer :fax
+      t.string :fl_resale_cert
+
+      t.timestamps
+    end
+  end
+end
